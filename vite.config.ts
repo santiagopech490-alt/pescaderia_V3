1ac950b9c95726610c13a909e9d4f139ae9ab0ad
hola0 <hola0@example.com>
2026-07-10 15:13:31 -0600

    chore: cambiar base path a pescaderia_V3

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/pescaderia/',
+  base: '/pescaderia_V3/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
